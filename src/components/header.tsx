2bc3ee163c18e91ff81140041402f6118016d6fd
Dmitry Rossomahin <rossomahin@rossomahin-2.local>
2026-01-12 06:10:58 +0300

    Fix a main card size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="flex justify-center font-thin my-12.5 font-sans text-3xl sm:text-8xl sm:tracking-[15px] tracking-[5px]">
+    <header className="flex justify-center font-thin mt-3 font-sans text-3xl sm:text-8xl sm:tracking-[15px] tracking-[5px]">
       <h1>#POMODOROTIMER</h1>
     </header>
   );
